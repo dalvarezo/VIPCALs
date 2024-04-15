f6cfddd4e0b6f6d3e5f6b25a8f3d6a32eb23c88f
Alvarez-Ortega Diego <dalvarez@ia.forth.gr>
2024-04-15 16:33:46 +0300

    Added script parallactic angle correction

---
+++ src/vipcals/scripts/pang_corr.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+
+from AIPS import AIPS
+from AIPSTask import AIPSTask, AIPSList
+
+def pang_corr(data):
+    """Correct phases for parallactic angles.
+    
+    Creates CL#6
+    """    
+    clcor = AIPSTask('clcor')
+    clcor.inname = data.name
+    clcor.inclass = data.klass
+    clcor.indisk = data.disk
+    clcor.inseq = data.seq
+    clcor.opcode = 'PANG'
+    clcor.clcorprm[1] = 1 # add corrections, remove them if < 0
+    clcor.msgkill = -4
+    
+    clcor.go()
