2d3b29a14f0c7edbf4dff4f9ae8a466d66247a10
Diego Alvarez-Ortega <dalvarez@ia.forth.gr>
2024-04-17 12:39:39 +0300

    Added extra scripts

---
+++ src/vipcals/scripts/display.py
@@ -0,0 +1,35 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask, AIPSList
+
+import numpy as np
+import os
+
+def print_box(s):
+    box_width = 80
+
+    # Split the input string into lines to fit within the box width
+    lines = [s[i:i+box_width-8] for i in range(0, len(s), box_width-8)]
+
+    # Create the box
+    box = '//' + '/' * (box_width - 6) + '//'
+
+    # Print the box with the lines centered inside
+    print(box)
+    for line in lines:
+        print(f'// {line.center(box_width - 8)} //')
+    print(box + '\n')
+    
+def write_box(log, s):
+    box_width = 100
+
+    # Split the input string into lines to fit within the box width
+    lines = [s[i:i+box_width-8] for i in range(0, len(s), box_width-8)]
+
+    # Create the box
+    box = '//' + '/' * (box_width - 6) + '//\n'
+
+    # Print the box with the lines centered inside
+    log.write('\n' + box)
+    for line in lines:
+        log.write(f'// {line.center(box_width - 8)} //\n')
+    log.write(box)
